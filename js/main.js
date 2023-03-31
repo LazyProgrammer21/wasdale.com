19d9da1b1896f6bdb4bb3194cb0b62a9f3e5eb85
lazyprogrammer <tamang.subash36@gmail.com>
2023-03-31 18:27:10 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#nav-placeholder").load("navbar.html");
+  });
+
+  $(function(){
+    $("#header-placeholder").load("header-secotion.html");
+  });
+
+  $(function(){
+    $("#footer-placeholder").load("footer.html");
+  });
+
